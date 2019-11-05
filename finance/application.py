60d6098c6c180234845766e46224c4c4ba58ec62
Grace Nellore <gracernellore@gmail.com>
2019-11-05 11:25:46 -0800

    quote - add html

--- finance/application.py
+++ finance/application.py
@@ -119,7 +119,11 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+    if request.method == "POST":
+        pass
+
+    else:
+        return render_template("quote.html") 
 
 
 @app.route("/register", methods=["GET", "POST"])
